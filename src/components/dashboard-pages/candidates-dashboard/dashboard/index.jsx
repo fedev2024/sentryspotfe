d5f58b6d823ee4009602866dfd0f6d4c66b88f03
Rishi kashyap <kashyap1strishi@gmail.com>
2024-11-08 20:16:52 +0530

    jobseeker dashboard styling issue done

--- src/components/dashboard-pages/candidates-dashboard/dashboard/index.jsx
+++ src/components/dashboard-pages/candidates-dashboard/dashboard/index.jsx
@@ -31,29 +31,29 @@ const Index = () => {
       {/* <!-- Dashboard --> */}
       <section className="user-dashboard">
         <div className="dashboard-outer">
-          <BreadCrumb title="Howdy, Jerome!!" />
+          {/* <BreadCrumb title="Howdy, Jerome!!" /> */}
           {/* breadCrumb */}
 
           <MenuToggler />
           {/* Collapsible sidebar button */}
 
-          <div className="row">
+          <div className="row ">
             <TopCardBlock />
           </div>
           {/* End .row top card block */}
 
-          <div className="row">
-            <div className="col-xl-7 col-lg-12">
-              {/* <!-- Graph widget --> */}
+          <div className="row ">
+            {/* <div className="col-xl-7 col-lg-12">
+              {/* <!-- Graph widget --> 
               <div className="graph-widget ls-widget">
                 <ProfileChart />
               </div>
-              {/* End profile chart */}
+              {/* End profile chart
             </div>
-            {/* End .col */}
+            
 
             <div className="col-xl-5 col-lg-12">
-              {/* <!-- Notification Widget --> */}
+              {/* <!-- Notification Widget --> 
               <div className="notification-widget ls-widget">
                 <div className="widget-title">
                   <h4>Notifications</h4>
@@ -62,9 +62,39 @@ const Index = () => {
                   <Notification />
                 </div>
               </div>
+            </div> */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="graph-widget ls-widget">
+              <ProfileChart />
             </div>
+            {/* End profile chart */}
+
+            <div className="notification-widget ls-widget">
+              <div className="widget-title">
+                <h4>Notifications</h4>
+              </div>
+              <div className="widget-content">
+                <Notification />
+              </div>
+            </div>
+          </div>
             {/* End .col */}
 
+            <div className="col-lg-12">
+              {/* <!-- applicants Widget --> */}
+              <div className="applicants-widget ls-widget">
+                <div className="widget-title">
+                  <h4>Jobs Applied Recently</h4>
+                </div>
+                <div className="widget-content">
+                  <div className="row">
+                    {/* <!-- Candidate block three --> */}
+
+                    <JobApplied />
+                  </div>
+                </div>
+              </div>
+            </div>
             <div className="col-lg-12">
               {/* <!-- applicants Widget --> */}
               <div className="applicants-widget ls-widget">
