e796a8ee8844e6e61e71cfcf019a5b05c597d582
fedev22024 <fe.dev2.ba2024@gmail.com>
2024-11-20 15:15:31 +0530

    new changes

--- src/components/header/DefaulHeader2.jsx
+++ src/components/header/DefaulHeader2.jsx
@@ -11,7 +11,20 @@ const DefaulHeader2 = () => {
   const dispatch = useDispatch();
   const { userToken } = useSelector((state) => state.auth);
   const [navbar, setNavbar] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
+  // Close the dropdown if clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.target.closest("#user-dropdown")) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
   const changeBackground = () => {
     if (window.scrollY >= 10) {
       setNavbar(true);
@@ -28,44 +41,51 @@ const DefaulHeader2 = () => {
     <>
       {!userToken && ( // Only show the top header if the user is not logged in
         <div className="TopHeader">
-        <div className="container">
-          <div className="TopMenu">
-            <div className="TopSocial hidden lg:flex justify-center">
-              <i className="fa-brands fa-facebook mx-2"></i>
-              <i className="fa-brands fa-linkedin-in mx-2"></i>
-              <i className="fa-brands fa-instagram mx-2"></i>
-            </div>
+          <div className="container">
             <div className="TopMenu">
-              <ul className="flex">
-                <li>
-                  <a href="/job-list-v7">
-                    <i className="fa-solid fa-compass"></i> Jobs
-                  </a>
-                </li>
-                <li>
-                  <a href="">
-                    <i className="fa-solid fa-medal"></i> Careers & Training
-                  </a>
-                </li>
-                <li className="border-l-2"></li>
-                <li>
-                  <a href="https://employer.sentryspot.co.uk/">
-                    <i className="fa-solid fa-user"></i> Post Job
-                  </a>
-                </li>
-              </ul>
+              <div className="TopSocial hidden lg:flex justify-center">
+                <i className="fa-brands fa-facebook mx-2"></i>
+                <i className="fa-brands fa-linkedin-in mx-2"></i>
+                <i className="fa-brands fa-instagram mx-2"></i>
+              </div>
+              <div className="TopMenu">
+                <ul className="flex">
+                  <li>
+                    <a href="/job-list-v7">
+                      <i className="fa-solid fa-compass"></i> Jobs
+                    </a>
+                  </li>
+                  <li>
+                    <a href="">
+                      <i className="fa-solid fa-medal"></i> Careers & Training
+                    </a>
+                  </li>
+                  <li className="border-l-2"></li>
+                  <li>
+                    <a href="https://employer.sentryspot.co.uk/">
+                      <i className="fa-solid fa-user"></i> Post Job
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      
       )}
-      <div className={`header ${navbar ? 'bg-white shadow-md' : 'bg-transparent'} transition-all duration-300`}>
+      <div
+        className={`header ${
+          navbar ? "bg-white shadow-md" : "bg-transparent"
+        } transition-all duration-300`}
+      >
         <div className="container flex justify-between items-center">
           {/* Sidebar for Mobile View */}
           <div className="md:hidden flex items-center">
             <Link to="/">
-              <img src="https://htmlsentryspot.vercel.app/img/company_logo.png" alt="Logo" className="h-14 " />
+              <img
+                src="https://htmlsentryspot.vercel.app/img/company_logo.png"
+                alt="Logo"
+                className="h-14 "
+              />
             </Link>
           </div>
 
@@ -74,7 +94,11 @@ const DefaulHeader2 = () => {
             <div className="header-menu flex items-center">
               <div className="header-logo flex items-center">
                 <Link to="/">
-                  <img src="https://htmlsentryspot.vercel.app/img/company_logo.png" alt="Logo" className="h-14" />
+                  <img
+                    src="https://htmlsentryspot.vercel.app/img/company_logo.png"
+                    alt="Logo"
+                    className="h-14"
+                  />
                 </Link>
                 <div className="main-menu ms-4">
                   <ul className="flex space-x-4">
@@ -82,7 +106,9 @@ const DefaulHeader2 = () => {
                       <a href="/sentry-spot">AI Services</a>
                     </li>
                     <li>
-                      <a href="https://blog.sentryspot.co.uk/category/job-search-strategies/">Hiring Advice</a>
+                      <a href="https://blog.sentryspot.co.uk/category/job-search-strategies/">
+                        Hiring Advice
+                      </a>
                     </li>
                     <li>
                       <a href="/job-list-v7#tab2">Companies</a>
@@ -97,35 +123,108 @@ const DefaulHeader2 = () => {
           <div className="side-menu flex items-center">
             <div className="btn-box flex items-center">
               {userToken ? (
-             <>
-              
-              
-                <span className="icon la la-bell text-3xl  text-blue-900"></span>
-            
-             
-                <i class="las la-comment text-3xl mx-4 text-blue-900"></i>
-             
-            
-                <i class="las la-user text-3xl text-blue-900"></i>
-            
-                <Button
-                  className="bg-gray-500 p-3 duration-500 hover:bg-[#E60278] ml-4 "
-                  title="logout"
-                  onClick={() => {
-                    dispatch(logout());
-                  }}
-                >
-                  <IoLogOutOutline size={24} />
-                </Button>
-             </>
+                <>
+                  <span className="icon la la-bell text-3xl  text-blue-900"></span>
+
+                  <i class="las la-comment text-3xl mx-4 text-blue-900"></i>
+
+                  {/* <i class="las la-user text-3xl text-blue-900"></i>
+                   */}
+                  <div id="user-dropdown" className=" ">
+                    <i
+                      className="las la-user text-3xl text-blue-900 me-3 flex items-center focus:outline-none cursor-pointer"
+                      onClick={() => setIsOpen(!isOpen)}
+                    ></i>
+
+                    {isOpen && (
+                      <div className=" profile-dropdown absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-lg shadow-lg z-50">
+                        <ul className="py-2 text-sm text-gray-700 ">
+                          <li>
+                            <a
+                              href="#"
+                              className="block px-4 py-2 hover:bg-gray-100  "
+                            >
+                              Profile
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="#"
+                              className="block px-4 py-2 hover:bg-gray-100  "
+                            >
+                              Saved Searches
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="#"
+                              className="block px-4 py-2 hover:bg-gray-100  "
+                            >
+                              Saved Jobs
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="#"
+                              className="block px-4 py-2 hover:bg-gray-100  "
+                            >
+                              Applied Jobs
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="#"
+                              className="block px-4 py-2 hover:bg-gray-100  "
+                            >
+                              Recommended Jobs
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="#"
+                              className="block px-4 py-2 hover:bg-gray-100  "
+                            >
+                              Settings
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="#"
+                              className="block px-4 py-2  hover:bg-gray-100  "
+                            >
+                              Sign Out
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    )}
+                  </div>
+                  <Button
+                    className="bg-gray-500 p-3 duration-500 hover:bg-[#E60278] ml-4 "
+                    title="logout"
+                    onClick={() => {
+                      dispatch(logout());
+                    }}
+                  >
+                    <IoLogOutOutline size={24} />
+                  </Button>
+                </>
               ) : (
-                <a href="#" data-bs-toggle="modal" data-bs-target="#loginPopupModal" className="mr-2 my-4">
+                <a
+                  href="#"
+                  data-bs-toggle="modal"
+                  data-bs-target="#loginPopupModal"
+                  className="mr-2 my-4"
+                >
                   Sign in
                 </a>
               )}
               {!userToken && (
                 <Link to={"/sentry-spot"}>
-                  <button type="button" className="register-btn hidden lg:flex ">
+                  <button
+                    type="button"
+                    className="register-btn hidden lg:flex "
+                  >
                     Create Your Sentry ID
                   </button>
                 </Link>
