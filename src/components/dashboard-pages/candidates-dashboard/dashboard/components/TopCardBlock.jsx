0c6f05ad6bb90802aeb3ca5d495af6147033e550
Rishi kashyap <kashyap1strishi@gmail.com>
2024-12-19 13:52:44 +0530

    saved and aplied job done and mobile optimization done

--- src/components/dashboard-pages/candidates-dashboard/dashboard/components/TopCardBlock.jsx
+++ src/components/dashboard-pages/candidates-dashboard/dashboard/components/TopCardBlock.jsx
@@ -34,7 +34,7 @@ const TopCardBlock = () => {
     <>
       {cardContent.map((item) => (
         <div
-          className="ui-block col-xl-3 col-lg-6 col-md-6 col-sm-12 p-2 py-5 "
+          className="ui-block col-xl-3 col-lg-6 col-md-6 col-sm-12 "
           key={item.id}
         >
           <div className={`ui-item ${item.uiClass}`}>
